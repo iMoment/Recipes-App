5460fc95d45f358e328178e873acc76a2ce5090d
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-06 01:47:25 +0900

    Implement addImage and createRecipe actions

--- Recipes/CreateRecipeVC.swift
+++ Recipes/CreateRecipeVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class CreateRecipeVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -30,5 +31,32 @@ class CreateRecipeVC: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
         imagePicker.dismissViewControllerAnimated(true, completion: nil)
         recipeImage.image = image
+    }
+    
+    @IBAction func addImage(sender: AnyObject!) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func createRecipe(sender: AnyObject!) {
+        if let title = recipeTitle.text where title != "" {
+            let app = UIApplication.sharedApplication().delegate as! AppDelegate
+            let context = app.managedObjectContext
+            let entity = NSEntityDescription.entityForName("Recipe", inManagedObjectContext: context)!
+            let recipe = Recipe(entity: entity, insertIntoManagedObjectContext: context)
+            recipe.title = title
+            recipe.ingredients = recipeIngredients.text
+            recipe.steps = recipeSteps.text
+            recipe.setRecipeImage(recipeImage.image!)
+            
+            context.insertObject(recipe)
+            
+            do {
+                try context.save()
+            } catch {
+                print("Could not save recipe.")
+            }
+            
+            self.navigationController?.popViewControllerAnimated(true)
+        }
     }
 }
