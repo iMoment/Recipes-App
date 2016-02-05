ea9f586969ebd08b1342cb719878c5a18302e4ae
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-05 20:40:45 +0900

    Set up core data

---
+++ Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Recipes
+//
+//  Created by Stanley Pan on 2/5/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Recipe: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
